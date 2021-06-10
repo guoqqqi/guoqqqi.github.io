74d02ca4718c2696af211957444d76812d17587f
guoqqqi <979918879@qq.com>
2021-06-10 17:32:18 +0800

    feat: added umi

---
+++ src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/guoqi/workspace/my-app/node_modules/umi/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange',],
+});
+
+export { plugin };
---
+++ src/layouts/index
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.css';
+
+export default ({ children }) => (
+    <React.StrictMode>
+        {children}
+    </React.StrictMode>
+);
