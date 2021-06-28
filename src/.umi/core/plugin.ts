7f99d6a3d694457e80e85d5712c9f95b9f8dab5e
guoqqqi <979918879@qq.com>
2021-06-28 11:42:16 +0800

    fix: update code

--- src/.umi/core/plugin.ts
+++ src/.umi/core/plugin.ts
@@ -1,17 +1,8 @@
 // @ts-nocheck
-import { Plugin } from "/Users/guoqi/workspace/my-app/node_modules/umi/node_modules/@umijs/runtime";
+import { Plugin } from '/Users/guoqi/workspace/my-app/node_modules/umi/node_modules/@umijs/runtime';
 
 const plugin = new Plugin({
-  validKeys: [
-    "modifyClientRenderOpts",
-    "patchRoutes",
-    "rootContainer",
-    "render",
-    "onRouteChange",
-    "getInitialState",
-    "initialStateConfig",
-    "request",
-  ],
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','getInitialState','initialStateConfig','request',],
 });
 
 export { plugin };
