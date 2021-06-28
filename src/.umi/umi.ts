b8d925c635c61d5752a520b34e2b26308efcddfc
guoqqqi <979918879@qq.com>
2021-06-28 17:25:47 +0800

    test

--- src/.umi/umi.ts
+++ src/.umi/umi.ts
@@ -1,15 +1,12 @@
 // @ts-nocheck
 import './core/polyfill';
-
+import '@@/core/devScripts';
 import { plugin } from './core/plugin';
 import './core/pluginRegister';
 import { createHistory } from './core/history';
 import { ApplyPluginsType } from '/Users/guoqi/workspace/my-app/node_modules/umi/node_modules/@umijs/runtime';
 import { renderClient } from '/Users/guoqi/workspace/my-app/node_modules/umi/node_modules/@umijs/renderer-react/dist/index.js';
-import { getRoutes } from './core/routes';
-
-
-
+import { getRoutes } from './core/routes'
 
 const getClientRender = (args: { hot?: boolean; routes?: any[] } = {}) => plugin.applyPlugins({
   key: 'render',
